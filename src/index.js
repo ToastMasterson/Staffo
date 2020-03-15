d8306bf1c09d0766ab597d3ce25e9048304019b7
ToastMasterson <tylercmasterson@gmail.com>
2020-03-15 13:17:29 -0600

    Removes Firestore

--- src/index.js
+++ src/index.js
@@ -1,37 +1,21 @@
 import React from 'react'
-import { render } from 'react-dom'
+import ReactDOM from 'react-dom'
 
 import App from './App'
 
 import { Provider } from 'react-redux'
-import createFirestoreInstance from 'redux-firestore'
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase'
-
 
 import './redux/index'
-import firebase from './firebase'
 
 import store from './redux/store/store'
 
 import * as serviceWorker from './serviceWorker'
 
-// const rrfProps = {
-//     firebase,
-//     config: firebase,
-//     dispatch: store.dispatch,
-//     createFirestoreInstance
-// }
-
-function Main(){
-    return(
-        <Provider store={store}>
-            {/* <ReactReduxFirebaseProvider> */}
-                <App />
-            {/* </ReactReduxFirebaseProvider> */}
-        </Provider>
-    )
-}
-
-render(<Main/>, document.getElementById('root'))
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>, 
+    document.getElementById('root')
+)
 
 serviceWorker.unregister()
--- src/redux/reducers/index.js
+++ src/redux/reducers/index.js
@@ -1,9 +1,8 @@
 import { combineReducers } from 'redux'
 import authReducer from  './auth'
 import { firebaseReducer } from 'react-redux-firebase'
-import { createFirestoreInstance, firestoreReducer } from 'redux-firestore'
 import employees from './employees'
 
-const rootReducer = combineReducers({ firebaseReducer, firestoreReducer, authReducer, employees })
+const rootReducer = combineReducers({ firebaseReducer, authReducer, employees })
 
 export default rootReducer
